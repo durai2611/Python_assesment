25c4b47e607acbe1481fb3f322289dce318bce80
durai2611 <U63254@ust.com>
2026-06-27 12:02:56 +0530

    save

--- level2.py
+++ level2.py
@@ -1,3 +1,8 @@
+#Exercise 1: List Comprehension Mastery
+words = ["apple", "bat", "cherry", "dog", "elderberry"]
+result = [w.upper() for w in words if len(w) >= 4]
+print(result)
+
 #Exercise 7: Palindrome Sentence
 sentence = "A man, a plan, a canal: Panama"
 
@@ -15,3 +20,13 @@ cleaned = "".join(chars)
 
 # Step 4: Check palindrome
 print(cleaned == cleaned[::-1])
+
+#Exercise 17: Age Calculator (Exact)
+from datetime import date
+from dateutil.relativedelta import relativedelta
+birthdate = date(1995, 5, 15)
+today = date(2026, 1, 2)
+age = relativedelta(today, birthdate)
+print(f"Age: {age.years} years, {age.months} months, {age.days} days")
+
+
