f1275a9aead03a8a7fc5000cf825b3c2d4cf3f35
durai2611 <U63254@ust.com>
2026-06-27 12:10:18 +0530

    a

--- level2.py
+++ level2.py
@@ -5,20 +5,12 @@ print(result)
 
 #Exercise 7: Palindrome Sentence
 sentence = "A man, a plan, a canal: Panama"
-
-# Step 1: Convert to lowercase
 sentence = sentence.lower()
-
-# Step 2: Collect valid characters in a list
 chars = []
 for ch in sentence:
     if ch.isalnum():   # keep only letters and digits
         chars.append(ch)
-
-# Step 3: Join the list into a string
 cleaned = "".join(chars)
-
-# Step 4: Check palindrome
 print(cleaned == cleaned[::-1])
 
 #Exercise 17: Age Calculator (Exact)
